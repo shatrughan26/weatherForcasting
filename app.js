8f9359c7bdacb8b29e1926b1dbc4a30c6fef3b84
Deepak kumar sharma <deepak4996sharma@gmail.com>
2026-05-19 22:50:15 +0530

    More HTMl and java script has to be done

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ import { env } from "./env.js";
 const searchBtn = document.querySelector("#search-btn");
 const searchInput = document.querySelector("#search-input");
 const API_Key = env.WETHER_API_KEY;
+const map = L.map('map').setView([20.5937, 78.9629], 5);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
 
 searchBtn.addEventListener("click", async () => {
     const city = searchInput.value;
@@ -16,5 +22,22 @@ searchBtn.addEventListener("click", async () => {
     console.log(data);
 
     document.querySelector("#temp").innerHTML = `${data.main.temp}°C`;
+
+    const geoUrl = `https://nominatim.openstreetmap.org/search?format=json&q=${city}`;
+
+    const geoResponse = await fetch(geoUrl);
+
+    const geoData = await geoResponse.json();
+
+    const lat = geoData[0].lat;
+    const lon = geoData[0].lon;
+
+    map.setView([lat, lon], 10);
+
+    L.marker([lat,lon]).addTo(map).bindPopup(city).openPopup();
+
+
 });
+
+
 
